4c59c593392e90c1e19a54b5bed926d51cda2925
snurby7 <ryan.bruns7@gmail.com>
2018-09-04 21:03:27 -0400

    minor styling for the home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,7 @@ import {
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
     public title = 'my-nab';
